faebb08bfa83b83a2da18e6c5017a81633e53bf9
Mazi98 <mazurarek242@gmail.com>
2022-06-03 13:16:37 +0200

    Revert "Add files via upload"
    This reverts commit 2fd6a960940e316def4aa4e47ab4184ec1849930.

--- form_menu_quiz/Start_form/RexExp.js
+++
@@ -1,8 +0,0 @@
-export const allRexExp = {
-
-     letters : /[a-z]/,
-     numbers : /[0-9]/,
-     specialSigns : /[!@#$%^^&*()]/,
-     RexExpEmail :  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
-
-}
