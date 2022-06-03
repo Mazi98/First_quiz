4b485b01e44a0f475d4acbc7b8a87a3522c1b4f8
Mazi98 <106081257+Mazi98@users.noreply.github.com>
2022-06-03 13:28:57 +0200

    Add files via upload

---
+++ form_menu_quiz/Start_form/RexExp.js
@@ -0,0 +1,8 @@
+export const allRexExp = {
+
+     letters : /[a-z]/,
+     numbers : /[0-9]/,
+     specialSigns : /[!@#$%^^&*()]/,
+     RexExpEmail :  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+
+}
